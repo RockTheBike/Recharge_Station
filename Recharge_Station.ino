12f467c012aa0f0e4903e468d2acc14f4af0f0fb
Rock The Bike <paul@rockthebike.com>
2015-09-30 23:24:46 -0700

    add setPwmFrequency() but don't use it yet

--- Recharge_Station.ino
+++ Recharge_Station.ino
@@ -88,6 +88,7 @@ void setup() {
 
   pinMode(RELAYPIN, OUTPUT);
   pinMode(BUCKPIN, OUTPUT);
+  // setPwmFrequency(BUCKPIN,8); // 31250 / 8 = 3906.25 Hz (default is 64 = 488.28125 Hz)
 
   ledStrip.begin(); // initialize the addressible LEDs
   ledStrip.show(); // clear their state
@@ -326,4 +327,63 @@ void printDisplay(){
   Serial.print(nowLedLevel);
   Serial.print("  lastLedLevel: ");
   Serial.println(lastLedLevel);
+}
+
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+    case 1:
+      mode = 0x01;
+      break;
+    case 8:
+      mode = 0x02;
+      break;
+    case 64:
+      mode = 0x03;
+      break;
+    case 256:
+      mode = 0x04;
+      break;
+    case 1024:
+      mode = 0x05;
+      break;
+    default:
+      return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    }
+    else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  }
+  else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+    case 1:
+      mode = 0x01;
+      break;
+    case 8:
+      mode = 0x02;
+      break;
+    case 32:
+      mode = 0x03;
+      break;
+    case 64:
+      mode = 0x04;
+      break;
+    case 128:
+      mode = 0x05;
+      break;
+    case 256:
+      mode = 0x06;
+      break;
+    case 1024:
+      mode = 0x7;
+      break;
+    default:
+      return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
 }
