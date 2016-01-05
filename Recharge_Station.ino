33ede8e02773f6369b39f7b02bd8f7bdfe76c4d9
Rock The Bike <paul@rockthebike.com>
2016-01-04 22:42:52 -0800

    everything works but Wheel() won't compile

--- Recharge_Station.ino
+++ Recharge_Station.ino
@@ -14,6 +14,9 @@ Adafruit_NeoPixel voltLedStrip = Adafruit_NeoPixel(NUM_VOLTLEDS, VOLTLEDSTRIPPIN
 #define NUM_WHATWATTPIXELS (NUM_POWER_PIXELS+NUM_ENERGY_PIXELS)  // number LEDs per bike
 Adafruit_NeoPixel whatWattStrip = Adafruit_NeoPixel(NUM_WHATWATTPIXELS, WHATWATTPIN, NEO_GRB + NEO_KHZ800);
 
+#define IND_BLINK_INTERVAL 300
+#define IND_VOLT_LOW 11 // handlebar pedalometer blinks red
+#define IND_VOLT_HIGH 28.0 // handlebar pedalometer blinks white
 #define ledBrightness 127 // brightness of addressible LEDs (0 to 255)
 
 #define VOLTS_CUTOUT 10 // disconnect from the ultracaps below this voltage
@@ -31,13 +34,43 @@ const float ledLevels[NUM_VOLTLEDS+1] = {
 #define AVG_CYCLES 50 // average measured values over this many samples
 #define DISPLAY_INTERVAL 2000 // when auto-display is on, display every this many milli-seconds
 #define ENERGY_INTERVAL 0
+#define IND_INTERVAL 100
 #define BLINK_PERIOD 600
 #define FAST_BLINK_PERIOD 150
 
+// scale the logarithmic displays:
+// (we assume a relaxed pedaler produces 60W)
+// barely turning the cranks produces 10W (not much more than noise)
+#define MIN_POWER 10
+// a sprinting athlete should just barely reach the top
+#define MAX_POWER 1000
+
+#ifndef WIMPIFY
+// 15sec of relaxed pedaling should trigger minimally visible glow
+#define MIN_ENERGY (float)(60*15)
+// earn a smoothie with equivalent of a half-hour of relaxed pedaling
+// ie 60W * 0.5hr * 3600s/hr
+#define MAX_ENERGY (float)(60*0.5*3600)
+#else
+// re-tune for testers and other wimps:  earn a smoothie in seconds!
+#define MIN_ENERGY (float)(60*5)
+#define MAX_ENERGY (float)(60*10)
+#endif
+
+uint32_t ENERGY_COLORS[] = {
+  Adafruit_NeoPixel::Color(0,0,0),
+  Adafruit_NeoPixel::Color(255,0,0),
+  Adafruit_NeoPixel::Color(0,255,0),
+  Adafruit_NeoPixel::Color(255,0,255),
+  Adafruit_NeoPixel::Color(255,128,0),
+  Adafruit_NeoPixel::Color(0,255,255) };
+#define NUM_ENERGY_COLORS (sizeof(ENERGY_COLORS)/sizeof(*ENERGY_COLORS))
+
 #define STATE_OFF 0
-#define STATE_BLINK 1
-#define STATE_BLINKFAST 3
-#define STATE_ON 2
+#define STATE_ON 1
+#define STATE_BLINK_LOW 2
+#define STATE_BLINK_HIGH 3
+#define STATE_RAMP 4
 
 #define MAX_VOLTS 29  // when to open the safety relay
 #define RECOVERY_VOLTS 25  // when to close the safety relay
@@ -73,6 +106,8 @@ unsigned long timeFastBlink = 0;
 unsigned long timeBlink = 0;
 unsigned long timeDisplay = 0;
 unsigned long lastEnergy = 0;
+unsigned long lastIndicatorTime = 0;
+int indState = STATE_RAMP;
 
 // var for looping through arrays
 int i = 0;
@@ -123,6 +158,132 @@ void loop() {
     doEnergy();
   }
 
+  if(time - lastIndicatorTime > IND_INTERVAL){
+    lastIndicatorTime = time;
+    doIndicators();
+  }
+
+}
+/*
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(const Adafruit_NeoPixel& strip, byte WheelPos) {
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, WheelPos * 3, 0);
+  } else if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, 255 - WheelPos * 3, WheelPos * 3);
+  } else {
+    WheelPos -= 170;
+    return strip.Color(WheelPos * 3, 0, 255 - WheelPos * 3);
+  }
+}
+*/
+void doIndBlink(){
+
+  boolean indBlinkState = (time % (IND_BLINK_INTERVAL * 2) > IND_BLINK_INTERVAL);
+
+  // turn all pixels off:
+  if(!indBlinkState){
+    for (byte i = 0; i < whatWattStrip.numPixels(); i++) whatWattStrip.setPixelColor(i,0,0,0);
+  } else {
+    if(indState == STATE_BLINK_LOW){
+      for (byte i = 0; i < whatWattStrip.numPixels(); i++) whatWattStrip.setPixelColor(i,255,0,0);
+    }else if(indState == STATE_BLINK_HIGH){
+      for (byte i = 0; i < whatWattStrip.numPixels(); i++) whatWattStrip.setPixelColor(i,255,255,255);
+    }
+  }
+}
+
+void doIndRamp(){
+  float ledstolight;
+
+  // the power LEDs
+  ledstolight = logPowerRamp(watts);
+  if( ledstolight > NUM_POWER_PIXELS ) ledstolight=NUM_POWER_PIXELS;
+  unsigned char hue = ledstolight/NUM_POWER_PIXELS * 170.0;
+  uint32_t color = 5;//Wheel(whatWattStrip, hue<1?1:hue);
+  static const uint32_t dark = Adafruit_NeoPixel::Color(0,0,0);
+  doFractionalRamp(0, NUM_POWER_PIXELS, ledstolight, color, dark);
+
+  // the energy LEDs
+  int full_smoothies = energy/MAX_ENERGY;
+  float partial_smoothie = energy - full_smoothies * MAX_ENERGY;
+  ledstolight = logEnergyRamp(partial_smoothie);
+  if( ledstolight > NUM_ENERGY_PIXELS ) ledstolight=NUM_ENERGY_PIXELS;
+  uint32_t curcolor = ENERGY_COLORS[full_smoothies%NUM_ENERGY_COLORS];
+  uint32_t nextcolor = ENERGY_COLORS[(full_smoothies+1)%NUM_ENERGY_COLORS];
+  doBackwardsFractionalRamp(NUM_POWER_PIXELS, NUM_ENERGY_PIXELS,
+    ledstolight, nextcolor, curcolor );
+
+  // and show 'em
+  whatWattStrip.show();
+}
+
+void doFractionalRamp(uint8_t offset, uint8_t num_pixels, float ledstolight, uint32_t firstColor, uint32_t secondColor){
+  for( int i=0,pixel=offset; i<=num_pixels; i++,pixel++ ){
+    uint32_t color;
+    if( i<(int)ledstolight )  // definitely firstColor
+        color = firstColor;
+    else if( i>(int)ledstolight )  // definitely secondColor
+        color = secondColor;
+    else  // mix the two proportionally
+        color = weighted_average_of_colors( firstColor, secondColor, ledstolight-(int)ledstolight);
+    whatWattStrip.setPixelColor(pixel, color);
+  }
+}
+
+// useful for the upside-down energy LEDs
+void doBackwardsFractionalRamp(uint8_t offset, uint8_t num_pixels, float ledstolight, uint32_t firstColor, uint32_t secondColor){
+  doFractionalRamp(offset, num_pixels, num_pixels-ledstolight, secondColor, firstColor);
+}
+
+// Yay, a closed form solution, and it's even got meaningful parameters!
+
+float logPowerRamp( float p ) {
+  float l = log(p/MIN_POWER)*NUM_POWER_PIXELS/log(MAX_POWER/MIN_POWER);
+  return l<0 ? 0 : l;
+}
+
+float logEnergyRamp( float e ) {
+  float l = log(e/MIN_ENERGY)*NUM_ENERGY_PIXELS/log(MAX_ENERGY/MIN_ENERGY);
+  return l<0 ? 0 : l;
+}
+
+void doIndicators(){
+
+  if(volts < IND_VOLT_LOW){
+    indState = STATE_BLINK_LOW;
+  } else if(volts < IND_VOLT_HIGH){
+    indState = STATE_RAMP;
+  } else if (volts >= IND_VOLT_HIGH){
+    indState = STATE_BLINK_HIGH;
+  }
+
+  if(indState == STATE_RAMP){
+    doIndRamp();
+  } else {
+    doIndBlink();
+  }
+}
+
+// hacky utility to merge colors
+// fraction=0 => colorA; fraction=1 => colorB; fraction=0.5 => mix
+// TODO:  but something's backward in the code or my brain!
+// (let's hope Adafruit_NeoPixel doesn't change its encoding of colors)
+uint32_t weighted_average_of_colors( uint32_t colorA, uint32_t colorB,
+  float fraction ){
+  // TODO:  weight brightness to look more linear to the human eye
+  uint8_t RA = (colorA>>16) & 0xff;
+  uint8_t GA = (colorA>>8 ) & 0xff;
+  uint8_t BA = (colorA>>0 ) & 0xff;
+  uint8_t RB = (colorB>>16) & 0xff;
+  uint8_t GB = (colorB>>8 ) & 0xff;
+  uint8_t BB = (colorB>>0 ) & 0xff;
+  return Adafruit_NeoPixel::Color(
+    RA*fraction + RB*(1-fraction),
+    GA*fraction + GB*(1-fraction),
+    BA*fraction + BB*(1-fraction) );
 }
 
 void doEnergy(){
